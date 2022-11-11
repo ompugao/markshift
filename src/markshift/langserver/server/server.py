2263678d3fed8c539a8719eda3c7e9e6565c5258
Shohei Fujii <fujii.shohei@gmail.com>
2022-11-11 12:46:51 +0800

    add example server

---
+++ src/markshift/langserver/server/server.py
@@ -0,0 +1,336 @@
+############################################################################
+# Copyright(c) Open Law Library. All rights reserved.                      #
+# See ThirdPartyNotices.txt in the project root for additional notices.    #
+#                                                                          #
+# Licensed under the Apache License, Version 2.0 (the "License")           #
+# you may not use this file except in compliance with the License.         #
+# You may obtain a copy of the License at                                  #
+#                                                                          #
+#     http: // www.apache.org/licenses/LICENSE-2.0                         #
+#                                                                          #
+# Unless required by applicable law or agreed to in writing, software      #
+# distributed under the License is distributed on an "AS IS" BASIS,        #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+# See the License for the specific language governing permissions and      #
+# limitations under the License.                                           #
+############################################################################
+import sys, os
+sys.path.append(os.path.abspath(''))
+import re
+import threading
+import logging
+from functools import partial
+try:
+    import ujson as json
+except Exception:  # pylint: disable=broad-except
+    import json
+import webview
+
+import asyncio
+# import json
+import re
+import time
+import uuid
+from typing import Optional
+
+from pygls.lsp.methods import (COMPLETION, TEXT_DOCUMENT_DID_CHANGE,
+                               TEXT_DOCUMENT_DID_CLOSE, TEXT_DOCUMENT_DID_OPEN, 
+                               TEXT_DOCUMENT_SEMANTIC_TOKENS_FULL)
+from pygls.lsp.types import (CompletionItem, CompletionList, CompletionOptions,
+                             CompletionParams, ConfigurationItem,
+                             ConfigurationParams, Diagnostic,
+                             DidChangeTextDocumentParams,
+                             DidCloseTextDocumentParams,
+                             DidOpenTextDocumentParams, MessageType, Position,
+                             Range, Registration, RegistrationParams,
+                             SemanticTokens, SemanticTokensLegend, SemanticTokensParams,
+                             Unregistration, UnregistrationParams)
+from pygls.lsp.types.basic_structures import (WorkDoneProgressBegin,
+                                              WorkDoneProgressEnd,
+                                              WorkDoneProgressReport)
+from pygls.server import LanguageServer
+
+COUNT_DOWN_START_IN_SECONDS = 10
+COUNT_DOWN_SLEEP_IN_SECONDS = 1
+
+
+class MarkshiftLanguageServer(LanguageServer):
+    CMD_COUNT_DOWN_BLOCKING = 'countDownBlocking'
+    CMD_COUNT_DOWN_NON_BLOCKING = 'countDownNonBlocking'
+    CMD_PROGRESS = 'progress'
+    CMD_REGISTER_COMPLETIONS = 'registerCompletions'
+    CMD_SHOW_CONFIGURATION_ASYNC = 'showConfigurationAsync'
+    CMD_SHOW_CONFIGURATION_CALLBACK = 'showConfigurationCallback'
+    CMD_SHOW_CONFIGURATION_THREAD = 'showConfigurationThread'
+    CMD_UNREGISTER_COMPLETIONS = 'unregisterCompletions'
+
+    CONFIGURATION_SECTION = 'MarkshiftLanguageServer'
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+        self.window = None
+
+    # def start_io(self, stdin=None, stdout=None):
+    #     super().start_io()
+    #     self._start_hook()
+
+    # def start_pyodide(self):
+    #     super().start_pyodide()
+    #     self._start_hook()
+
+    # def start_tcp(self, host, port):
+    #     super().start_tcp()
+    #     self._start_hook()
+
+    # def start_ws(self, host, port):
+    #     super().start_ws()
+    #     self._start_hook()
+
+    def shutdown(self,):
+        super().shutdown()
+        if self.window is not None:
+            self.window.destroy()
+
+
+msls_server = MarkshiftLanguageServer('pygls-json-example', 'v0.1')
+
+def _validate(ls, params):
+    ls.show_message_log('Validating json...')
+
+    text_doc = ls.workspace.get_document(params.text_document.uri)
+
+    source = text_doc.source
+    diagnostics = _validate_json(source) if source else []
+
+    ls.publish_diagnostics(text_doc.uri, diagnostics)
+
+
+def _validate_json(source):
+    """Validates json file."""
+    diagnostics = []
+
+    try:
+        json.loads(source)
+    except JSONDecodeError as err:
+        msg = err.msg
+        col = err.colno
+        line = err.lineno
+
+        d = Diagnostic(
+            range=Range(
+                start=Position(line=line - 1, character=col - 1),
+                end=Position(line=line - 1, character=col)
+            ),
+            message=msg,
+            source=type(msls_server).__name__
+        )
+
+        diagnostics.append(d)
+
+    return diagnostics
+
+
+@msls_server.feature(COMPLETION, CompletionOptions(trigger_characters=[',']))
+def completions(params: Optional[CompletionParams] = None) -> CompletionList:
+    """Returns completion items."""
+    return CompletionList(
+        is_incomplete=False,
+        items=[
+            CompletionItem(label='"'),
+            CompletionItem(label='['),
+            CompletionItem(label=']'),
+            CompletionItem(label='{'),
+            CompletionItem(label='}'),
+        ]
+    )
+
+
+@msls_server.command(MarkshiftLanguageServer.CMD_COUNT_DOWN_BLOCKING)
+def count_down_10_seconds_blocking(ls, *args):
+    """Starts counting down and showing message synchronously.
+    It will `block` the main thread, which can be tested by trying to show
+    completion items.
+    """
+    for i in range(COUNT_DOWN_START_IN_SECONDS):
+        ls.show_message(f'Counting down... {COUNT_DOWN_START_IN_SECONDS - i}')
+        time.sleep(COUNT_DOWN_SLEEP_IN_SECONDS)
+
+
+@msls_server.command(MarkshiftLanguageServer.CMD_COUNT_DOWN_NON_BLOCKING)
+async def count_down_10_seconds_non_blocking(ls, *args):
+    """Starts counting down and showing message asynchronously.
+    It won't `block` the main thread, which can be tested by trying to show
+    completion items.
+    """
+    for i in range(COUNT_DOWN_START_IN_SECONDS):
+        ls.show_message(f'Counting down... {COUNT_DOWN_START_IN_SECONDS - i}')
+        await asyncio.sleep(COUNT_DOWN_SLEEP_IN_SECONDS)
+
+
+@msls_server.feature(TEXT_DOCUMENT_DID_CHANGE)
+def did_change(ls, params: DidChangeTextDocumentParams):
+    """Text document did change notification."""
+    _validate(ls, params)
+
+
+@msls_server.feature(TEXT_DOCUMENT_DID_CLOSE)
+def did_close(server: MarkshiftLanguageServer, params: DidCloseTextDocumentParams):
+    """Text document did close notification."""
+    server.show_message('Text Document Did Close')
+
+
+@msls_server.feature(TEXT_DOCUMENT_DID_OPEN)
+async def did_open(ls, params: DidOpenTextDocumentParams):
+    """Text document did open notification."""
+    ls.show_message('Text Document Did Open')
+    _validate(ls, params)
+
+
+@msls_server.feature(
+    TEXT_DOCUMENT_SEMANTIC_TOKENS_FULL,
+    SemanticTokensLegend(
+        token_types = ["operator"],
+        token_modifiers = []
+    )
+)
+def semantic_tokens(ls: MarkshiftLanguageServer, params: SemanticTokensParams):
+    """See https://microsoft.github.io/language-server-protocol/specification#textDocument_semanticTokens
+    for details on how semantic tokens are encoded."""
+    
+    TOKENS = re.compile('".*"(?=:)')
+    
+    uri = params.text_document.uri
+    doc = ls.workspace.get_document(uri)
+
+    last_line = 0
+    last_start = 0
+
+    data = []
+
+    for lineno, line in enumerate(doc.lines):
+        last_start = 0
+
+        for match in TOKENS.finditer(line):
+            start, end = match.span()
+            data += [
+                (lineno - last_line),
+                (start - last_start),
+                (end - start),
+                0, 
+                0
+            ]
+
+            last_line = lineno
+            last_start = start
+
+    return SemanticTokens(data=data)
+
+
+
+@msls_server.command(MarkshiftLanguageServer.CMD_PROGRESS)
+async def progress(ls: MarkshiftLanguageServer, *args):
+    """Create and start the progress on the client."""
+    token = 'token'
+    # Create
+    await ls.progress.create_async(token)
+    # Begin
+    ls.progress.begin(token, WorkDoneProgressBegin(title='Indexing', percentage=0))
+    # Report
+    for i in range(1, 10):
+        ls.progress.report(
+            token,
+            WorkDoneProgressReport(message=f'{i * 10}%', percentage= i * 10),
+        )
+        await asyncio.sleep(2)
+    # End
+    ls.progress.end(token, WorkDoneProgressEnd(message='Finished'))
+
+
+@msls_server.command(MarkshiftLanguageServer.CMD_REGISTER_COMPLETIONS)
+async def register_completions(ls: MarkshiftLanguageServer, *args):
+    """Register completions method on the client."""
+    params = RegistrationParams(registrations=[
+                Registration(
+                    id=str(uuid.uuid4()),
+                    method=COMPLETION,
+                    register_options={"triggerCharacters": "[':']"})
+             ])
+    response = await ls.register_capability_async(params)
+    if response is None:
+        ls.show_message('Successfully registered completions method')
+    else:
+        ls.show_message('Error happened during completions registration.',
+                        MessageType.Error)
+
+
+@msls_server.command(MarkshiftLanguageServer.CMD_SHOW_CONFIGURATION_ASYNC)
+async def show_configuration_async(ls: MarkshiftLanguageServer, *args):
+    """Gets exampleConfiguration from the client settings using coroutines."""
+    try:
+        config = await ls.get_configuration_async(
+            ConfigurationParams(items=[
+                ConfigurationItem(
+                    scope_uri='',
+                    section=MarkshiftLanguageServer.CONFIGURATION_SECTION)
+        ]))
+
+        example_config = config[0].get('exampleConfiguration')
+
+        ls.show_message(f'jsonServer.exampleConfiguration value: {example_config}')
+
+    except Exception as e:
+        ls.show_message_log(f'Error ocurred: {e}')
+
+
+@msls_server.command(MarkshiftLanguageServer.CMD_SHOW_CONFIGURATION_CALLBACK)
+def show_configuration_callback(ls: MarkshiftLanguageServer, *args):
+    """Gets exampleConfiguration from the client settings using callback."""
+    def _config_callback(config):
+        try:
+            example_config = config[0].get('exampleConfiguration')
+
+            ls.show_message(f'jsonServer.exampleConfiguration value: {example_config}')
+
+        except Exception as e:
+            ls.show_message_log(f'Error ocurred: {e}')
+
+    ls.get_configuration(ConfigurationParams(items=[
+        ConfigurationItem(
+            scope_uri='',
+            section=MarkshiftLanguageServer.CONFIGURATION_SECTION)
+    ]), _config_callback)
+
+
+@msls_server.thread()
+@msls_server.command(MarkshiftLanguageServer.CMD_SHOW_CONFIGURATION_THREAD)
+def show_configuration_thread(ls: MarkshiftLanguageServer, *args):
+    """Gets exampleConfiguration from the client settings using thread pool."""
+    try:
+        config = ls.get_configuration(ConfigurationParams(items=[
+            ConfigurationItem(
+                scope_uri='',
+                section=MarkshiftLanguageServer.CONFIGURATION_SECTION)
+        ])).result(2)
+
+        example_config = config[0].get('exampleConfiguration')
+
+        ls.show_message(f'jsonServer.exampleConfiguration value: {example_config}')
+
+    except Exception as e:
+        ls.show_message_log(f'Error ocurred: {e}')
+
+
+@msls_server.command(MarkshiftLanguageServer.CMD_UNREGISTER_COMPLETIONS)
+async def unregister_completions(ls: MarkshiftLanguageServer, *args):
+    """Unregister completions method on the client."""
+    params = UnregistrationParams(unregisterations=[
+        Unregistration(id=str(uuid.uuid4()), method=COMPLETION)
+    ])
+    response = await ls.unregister_capability_async(params)
+    if response is None:
+        ls.show_message('Successfully unregistered completions method')
+    else:
+        ls.show_message('Error happened during completions unregistration.',
+                        MessageType.Error)
+
