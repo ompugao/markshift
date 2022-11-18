ac3fa888cec11a12398559fad3c639bb7de56e14
Shohei Fujii <fujii.shohei@gmail.com>
2022-11-18 10:37:35 +0800

    add options to langserver

--- src/markshift/langserver/server/__main__.py
+++ src/markshift/langserver/server/__main__.py
@@ -21,7 +21,6 @@ import webview
 
 from .server import msls_server, Api
 
-logging.basicConfig(filename="msls.log", level=logging.DEBUG, filemode="w")
 
 log = logging.getLogger(__name__)
 
@@ -44,27 +43,48 @@ def add_arguments(parser):
         "--port", type=int, default=2087,
         help="Bind to this port"
     )
+    parser.add_argument(
+        "--logfile", type=str, default='',
+        help="path to log file"
+    )
+    parser.add_argument(
+        "--always_on_top", action='store_true',
+        help=""
+    )
+    parser.add_argument(
+        "--hidden_on_boot", action='store_true',
+        help="do not show a preview window when started"
+    )
+    parser.add_argument(
+        "--never_steal_focus", action='store_true',
+        help="set a previewer not to steal focus"
+    )
 
 def main():
     parser = argparse.ArgumentParser()
     add_arguments(parser)
     args = parser.parse_args()
 
+    if len(args.logfile) > 0:
+        logging.basicConfig(filename=args.logfile, level=logging.DEBUG, filemode="w")
     api = Api(msls_server)
-    msls_server.window = webview.create_window('markshift_previewer', js_api=api, hidden=True)
+    msls_server.window = webview.create_window('markshift_previewer', js_api=api, hidden=True, text_select=True, on_top=args.always_on_top)
     webview.start(_start, args, gui='qt')
     # _start(args)
 
 def _start(args):
-    import time
-    time.sleep(1)
+    if args.never_steal_focus:
+        # do not steal focus
+        import time
+        time.sleep(1)
 
-    # do not steal focus
-    # from qtpy.QtCore import Qt
-    # view = msls_server.window.gui.BrowserView.instances['master']
-    # view.setWindowFlag(Qt.WindowDoesNotAcceptFocus)
-    # time.sleep(0.1)
-    msls_server.window.show()
+        from qtpy.QtCore import Qt
+        view = msls_server.window.gui.BrowserView.instances['master']
+        view.setWindowFlag(Qt.WindowDoesNotAcceptFocus)
+        time.sleep(0.1)
+
+    if not args.hidden_on_boot:
+        msls_server.window.show()
 
     if args.tcp:
         msls_server.start_tcp(args.host, args.port)
