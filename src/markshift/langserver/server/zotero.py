1cd27c0b071343bd471eada423e32530eeb1d42a
Shohei Fujii <fujii.shohei@gmail.com>
2022-11-22 10:52:26 +0800

    copy zotero db to temporary file

--- src/markshift/langserver/server/zotero.py
+++ src/markshift/langserver/server/zotero.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
-import sqlite3
-import json
-import pathlib
+from contextlib import ExitStack, closing
 import functools
+import json
 import logging
+import tempfile
+import pathlib
+import shutil
+import sqlite3
 
 log = logging.getLogger(__name__)
 
@@ -13,22 +16,26 @@ def zotero_comp(ls, zotero_path = '~/Zotero'):
     path = (pathlib.Path(zotero_path) / ('zotero.sqlite')).expanduser()
     if not path.exists():
         return []
-    conn = sqlite3.connect(path)
-    cur = conn.cursor()
-    try:
-        cachedata = cur.execute("SELECT data from syncCache").fetchall()
-    except sqlite3.OperationalError as e:
-        mes = 'Zotero database cannot be accessed : %s'%str(e)
-        log.error(mes)
-        ls.show_message(mes)
-        return []
-    jsdata = [json.loads(d[0]) for d in cachedata]
-    papers = []
-    for d in jsdata:
-        if ('itemType' in d['data'].keys()):
-            if d['data']['itemType'] in ['conferencePaper', 'journalArticle', 'preprint', 'thesis']:
-                papers.append(d)
-    compitems = [(p['data']['title'], p['data']['title'] + ' zotero://select/library/items/' + p['key']) for p in papers]
-    cur.close()
-    conn.close()
+
+    with ExitStack() as stack:
+        dir = stack.enter_context(tempfile.TemporaryDirectory())
+        path_copied = pathlib.Path(dir) / 'zotero.db.msls'
+        shutil.copy(path, path_copied)
+
+        conn = stack.enter_context(closing(sqlite3.connect(path_copied)))
+        cur = stack.enter_context(conn)
+        try:
+            cachedata = cur.execute("SELECT data from syncCache").fetchall()
+        except sqlite3.OperationalError as e:
+            mes = 'Zotero database cannot be accessed : %s'%str(e)
+            log.error(mes)
+            ls.show_message(mes)
+            return []
+        jsdata = [json.loads(d[0]) for d in cachedata]
+        papers = []
+        for d in jsdata:
+            if ('itemType' in d['data'].keys()):
+                if d['data']['itemType'] in ['conferencePaper', 'journalArticle', 'preprint', 'thesis']:
+                    papers.append(d)
+        compitems = [(p['data']['title'], p['data']['title'] + ' zotero://select/library/items/' + p['key']) for p in papers]
     return compitems
